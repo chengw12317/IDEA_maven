cfd8291c4af25685aebec8e11b6cd158f025e3b6
chengw12317 <1183313963@qq.com>
2021-06-12 18:01:42 +0800

    test2

--- src/test/java/com/test/IdeaTest.java
+++ src/test/java/com/test/IdeaTest.java
@@ -7,6 +7,6 @@ public class IdeaTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test123");
-
+        System.out.println("test1234");
     }
 }
