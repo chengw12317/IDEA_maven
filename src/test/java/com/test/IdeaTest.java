3202abf826e6280d6ae7a78ce729b220fea89968
chengw12317 <1183313963@qq.com>
2021-06-12 17:53:11 +0800

    test1

---
+++ src/test/java/com/test/IdeaTest.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by chenguangwei on 2021/6/12.
+ */
+public class IdeaTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
